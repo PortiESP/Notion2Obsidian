58807c03779e757fcf0a57a95f68ed30174f05aa
Porti <portiesp@gmail.com>
2023-04-19 00:31:29 +0200

    Setup configuration view

--- src/render/scenes/tool_setup/Tool_setup.jsx
+++ src/render/scenes/tool_setup/Tool_setup.jsx
@@ -8,49 +8,87 @@ export default function Setup(props){
     return (<div className={sass.div__setup_wrap}>
         <h1 className={sass.title}>🧰Converter Setup🧰</h1>
         <div className={sass.div__info}>
-            <p className={sass.callout}>Here you can select some additional options for your conversion, some of them are optional, but others are required</p>
+            <p className={sass.callout}>Here you can select some additional options for your conversion</p>
         </div>
         <div className={sass.div__form}>
-            <p><i>Hover on each option to read more about what it does</i></p>
+            <p className={sass.tip}><i>TIP: Hover on each option to read more about what it does</i></p>
+
+
+
+            <h2>Restructure export</h2>
 
             <div className={sass.div__item} title={props.tips.nameHash}>
                 <input type="checkbox" onChange={()=> props.setSetupOptions(old => {return {...old, nameHash:!old.nameHash}})} checked={props.setupOptions.nameHash} id="ck--name-hash"/>
                 <label htmlFor="ck--name-hash">String hash from file names </label>
             </div>
+            { props.setupOptions.nameHash && <p className={sass.example}><span className={sass.code}>myNote b6d4gh6gh46dr4vzx328u8u.md</span> as <span className={sass.code}>myNote.md</span></p>}
+
             <div className={sass.div__item} title={props.tips.moveToFolder}>
                 <input type="checkbox" onChange={()=> props.setSetupOptions(old => {return {...old, moveToFolder:!old.moveToFolder}})} checked={props.setupOptions.moveToFolder} id="ck--move-to-folder"/>
                 <label htmlFor="ck--move-to-folder">Move files to folder with its name </label>
             </div>
-            <div className={sass.div__item} title={props.tips.wikilinks}>
-                <input type="checkbox" onChange={()=> props.setSetupOptions(old => {return {...old, wikilinks:!old.wikilinks}})} checked={props.setupOptions.wikilinks} id="ck--wikilinks"/>
-                <label htmlFor="ck--wikilinks">Format URL/Img as Wikilinks </label>
-            </div>
-            <div className={sass.div__item} title={props.tips.asterisks}>
-                <input type="checkbox" onChange={()=> props.setSetupOptions(old => {return {...old, asterisks:!old.asterisks}})} checked={props.setupOptions.asterisks} id="ck--asterisks"/>
-                <label htmlFor="ck--asterisks">Strip repetitive asterisks from bold strings </label>
-            </div>
-            <div className={sass.div__item} title={props.tips.callouts}>
-                <input type="checkbox" onChange={()=> props.setSetupOptions(old => {return {...old, callouts:!old.callouts}})} checked={props.setupOptions.callouts} id="ck--callouts"/>
-                <label htmlFor="ck--callouts">Format callouts for obsidian </label>
-            </div>
+            { props.setupOptions.moveToFolder && <p className={sass.example}><span className={sass.code}>./myNote.md</span> moved to <span className={sass.code}>./myNote/myNote.md</span></p>}
+
             <div className={sass.div__item} title={props.tips.missing}>
                 <input type="checkbox" onChange={()=> props.setSetupOptions(old => {return {...old, missing:!old.missing}})} checked={props.setupOptions.missing} id="ck--missing"/>
                 <label htmlFor="ck--missing">Generate a list with missing databases </label>
             </div>
+
+
+
+            <h2>Format elements</h2>
+
+            <div className={sass.div__item} title={props.tips.wikilinks}>
+                <input type="checkbox" onChange={()=> props.setSetupOptions(old => {return {...old, wikilinks:!old.wikilinks}})} checked={props.setupOptions.wikilinks} id="ck--wikilinks"/>
+                <label htmlFor="ck--wikilinks">Format URL/Img as Wikilinks </label>
+            </div>
+            <div className={[sass.div__item, sass.div__subitem].join(" ")} title={props.tips.wikilinksComments}>
+                <input type="checkbox" onChange={()=> props.setSetupOptions(old => {return {...old, wikilinksComments:!old.wikilinksComments}})} checked={props.setupOptions.wikilinks && props.setupOptions.wikilinksComments} id="ck--wikilinks-comments"/>
+                <label htmlFor="ck--wikilinks-comments">Keep original URLs as Obsidian MD comment</label>
+            </div>
+            { props.setupOptions.wikilinks && <p className={sass.example}><span className={sass.code}>![Alt text](https://...)</span> as <span className={sass.code}>![[Alt text]]{props.setupOptions.wikilinksComments && " %% https://..."}</span></p>}
+
+            <div className={sass.div__item} title={props.tips.asterisks}>
+                <input type="checkbox" onChange={()=> props.setSetupOptions(old => {return {...old, asterisks:!old.asterisks}})} checked={props.setupOptions.asterisks} id="ck--asterisks"/>
+                <label htmlFor="ck--asterisks">Strip repetitive asterisks from bold strings </label>
+            </div>
+            { props.setupOptions.asterisks && <p className={sass.example}><span className={sass.code}>********boldText********</span> as <span className={sass.code}>**boldText**</span></p>}
+
+            <div className={sass.div__item} title={props.tips.callouts}>
+                <input type="checkbox" onChange={()=> props.setSetupOptions(old => {return {...old, callouts:!old.callouts}})} checked={props.setupOptions.callouts} id="ck--callouts"/>
+                <label htmlFor="ck--callouts">Format callouts for obsidian </label>
+            </div>
+            { props.setupOptions.callouts && <p className={sass.example}><span className={sass.code}>{`<aside>My Callout</aside>`}</span> as <span className={sass.code}>{`> [!note]\\n> My Callout`}</span></p>}
+
+
+
+            <h2>Lists Formatting</h2>
+
             <div className={sass.div__item} title={props.tips.genListFiles}>
                 <input type="checkbox" onChange={()=> props.setSetupOptions(old => {return {...old, genListFiles:!old.genListFiles}})} checked={props.setupOptions.genListFiles} id="ck--generate-list-files"/>
-                <label htmlFor="ck--generate-list-files">Generate a list from SVG files (<i>This option uses the 'Dataview' plugin with javascript queries enabled</i>) </label>
+                <label htmlFor="ck--generate-list-files">Generate a list from SVG files <span className={sass.labelMsg}>(IMPORTANT: you must have the plugin 'Dataview' installed with the JavaScript Queries enabled)</span></label>
                 <br/>
             </div>
-            <div className={sass.div__item_input}>
-                <label>Add a prefix/sufix to the generated list file </label>
-                <input type="text" onChange={ e => props.setSetupOptions(old => {return {...old, listNamePrefix:e.target.value}})} value={props.setupOptions.listNamePrefix} placeholder="Prefix" title={props.tips.listNamePrefix}/>
-                <input type="text" onChange={ e => props.setSetupOptions(old => {return {...old, listNameSufix:e.target.value}})} value={props.setupOptions.listNameSufix} placeholder="Sufix" title={props.tips.listNameSufix}/>
-            </div>
+            {
+                props.setupOptions.genListFiles &&
+
+                <div className={sass.div__input_name_wrap}>
+                    <label>Add a prefix/sufix to the generated list file: </label>
+                    <div className={sass.div__input_name}>
+                        <input type="text" onChange={ e => props.setSetupOptions(old => {return {...old, listNamePrefix:e.target.value}})} value={props.setupOptions.listNamePrefix} placeholder="Prefix" title={props.tips.listNamePrefix}/>
+                        <span> + filename + </span>
+                        <input type="text" onChange={ e => props.setSetupOptions(old => {return {...old, listNameSufix:e.target.value}})} value={props.setupOptions.listNameSufix} placeholder="Sufix" title={props.tips.listNameSufix}/>
+                        <span>.md</span>
+                    </div>
+                    <p>Preview: <span className={sass.code}>{props.setupOptions.listNamePrefix}exampleList{props.setupOptions.listNameSufix}.md</span></p>
+                </div>
+            }
+
             <div className={sass.div__item} title={props.tips.metadata}>
                 <input type="checkbox" onChange={()=> props.setSetupOptions(old => {return {...old, metadata:!old.metadata}})} checked={props.setupOptions.metadata} id="ck--metadata"/>
-                <label htmlFor="ck--metadata">Format metadata as Obsidian's note metadata </label>
+                <label htmlFor="ck--metadata">Format Notions page attributes as Obsidian's note metadata <span className={sass.labelMsg}>(Mark this option to have addition information of the notes in the Dataview tables)</span></label>
             </div>
+
         </div>
     </div>)
 }
