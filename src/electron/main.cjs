241e8e409702b3b6acae3529ae2a6cf397ca88d5
Porti <portiesp@gmail.com>
2023-04-20 19:58:02 +0200

    Handle exit fix

--- src/electron/main.cjs
+++ src/electron/main.cjs
@@ -41,14 +41,14 @@ app.on("ready", ()=>{
 })
 
 
-// // Quit when all windows are closed
-// app.on('window-all-closed', () => {
-//     // On macOS it is common for applications and their menu bar
-//     // to stay active until the user quits explicitly with Cmd + Q
-//     if (process.platform !== 'darwin') {
-//       app.quit();
-//     }
-// });
+// Quit when all windows are closed
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+});
 
 
 
