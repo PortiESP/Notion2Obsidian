fcbff9bbc600288f2d342ac730799bc5e94fc287
Porti <portiesp@gmail.com>
2023-04-22 00:59:50 +0200

    Semmantic fix

--- src/render/scenes/show_results/Show_Results.jsx
+++ src/render/scenes/show_results/Show_Results.jsx
@@ -13,7 +13,7 @@ export default function ResultsPage({results}){
         <h1 className={sass.title}>🕷️Error Log🕷️</h1>
         <div className={sass.callout}>Here you can view and save a report of the errors returned by the scripts, this errors may require to be handled manualy</div>
         <div>
-            <p className={sass.p__msg}>Total logs that require to be handled manual <span>({results ? Object.values(results).reduce((accum, list)=>[...accum, ...list]).length : 0})</span></p>
+            <p className={sass.p__msg}>Total logs that require to be handled manualy <span>({results ? Object.values(results).reduce((accum, list)=>[...accum, ...list]).length : 0})</span></p>
             <p className={sass.p__msg}>Check this <a className={sass.a} onClick={()=>postOfficeSend("open-url", "https://github.com/PortiESP/Notion2Obsidian/tree/master/Docs/HowToHandleErrorLogs.md")}>document</a> to know what actions you must take to handle this logs</p>
         </div>
         <button onClick={download} className={sass.button__download}>💾 Save Report</button>
