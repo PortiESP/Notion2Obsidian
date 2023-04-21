0f6659b7a2dea618a606e528f3ccfe5f7499ee99
Porti <portiesp@gmail.com>
2023-04-21 19:21:06 +0200

    Callout algorithm suppots nested callouts

--- src/electron/algorithms/calloutFormatter.cjs
+++ src/electron/algorithms/calloutFormatter.cjs
@@ -14,51 +14,99 @@ module.exports = function calloutConverter(folderPath, debug=true) {
   // Get all files recursively in the folder with the specified extension
   const files = getAllFilesRecursively(folderPath).filter( filePath => path.parse(filePath).ext === ".md");
   
-  // Define RegEx pattern
-  const regex = /<aside>\n*([\s\S]*?)<\/aside>/gm;
-  
   // Loop through each file
-  files.forEach(file => {
+  files.forEach(filePath => {
 
-      try{
-        // Read file content
-        const content = fs.readFileSync(file, 'utf8');
-        
-        // Find all matches of the regular expression pattern in the content
-        const matchResults = [...content.matchAll(regex)];
-        
-        if (matchResults.length > 0) {
-          // If the pattern matches, replace the content
-          let updatedContent = content;
-          
-          // Loop through each match
-        matchResults.forEach(match => {
-          // Extract the substring matched by the capturing group
-          const grupo = match[1].replace(/\r?\n$/, '');
-          
-          // Replace line breaks with '> ' to format the substring as a blockquote
-          const parsed = `> [!note]\n> ${grupo.replace(/\n/g, '\n>')}`;
-          
-          // Replace the original match with the parsed content in the updated content
-          updatedContent = updatedContent.replace(match[0], parsed);
-        });
-        
-        // Write the updated content back to the file
-        fs.writeFileSync(file, updatedContent);
-        
-        /<\/?aside>/g.test(updatedContent) && errorLog.push({file, error: "There are still '<aside>' tags in the file, this can be because of callout that were inside other callouts"})
-        
-        // Print a message indicating the replacement was done
-        if (debug) console.log(`[+] Replacement performed in: ${file}`);
+    try{
+      // Read file content
+      const content = fs.readFileSync(filePath, 'utf8');
+      
+      // Find all matches of the regular expression pattern in the content
+      const updatedContent = calloutReplacingAlgorithm(content.replace("\r", ""))
+      
+      // Check algorithm result: null=No matches found in this file
+      if (updatedContent === null) {
+        if (debug) console.log(`[i] No changes made at: ${filePath}`);
       } else {
-        // If no matches found, print a message indicating no matches
-        if (debug) console.log(`[i] No matches found in: ${file}`);
+        fs.writeFileSync(filePath, updatedContent);
+        if (debug) console.log(`[+] Replacement performed in: ${filePath}`);
       }
+        
+      
     } catch(err){
-      errorLog.push({file: file, error: err})
+      errorLog.push({file: filePath, error: err})
     }
   });
   
   return errorLog;
 }
 
+
+/**
+ * 
+ * @param {string} content - String where we want to perform the algorithm
+ * 
+ */
+function calloutReplacingAlgorithm(content){
+
+  // Append to each tag with its depth level
+  let depth = 0
+  let updatedContent = content.replaceAll(/<\/?aside>/gm, (match) => {
+    if (match === "<aside>") {
+      depth += 1
+      return `<aside${depth}>` 
+    }
+    else {
+      depth -= 1
+      return `</aside${depth+1}>` 
+    } 
+  })
+  
+  // Replace tags until no one is left
+  while (/<aside(\d+)>\n?([\s\S]+?)\n?<\/aside\1>/gm.test(updatedContent)){
+    updatedContent = updatedContent.replaceAll(/<aside(\d+)>\n?([\s\S]+?)\n?<\/aside\1>/gm, (match, g1, g2) => {
+      const parsed = `> [!note]\n> ${g2.replaceAll(/\n/g, '\n> ')}`;
+      return parsed
+    })
+  }
+  
+
+  return updatedContent
+  
+}
+
+// const openLen = "<aside>".length
+
+// // Stack of tags possition index
+// let openPos = [content.indexOf("<aside>"),]
+
+// let depth = 1
+// while (depth > 0){
+//   // Find next tags from the current one
+//   let nextOpenPos = content.indexOf("<aside>", openPos+openLen)
+//   let nextEndPos = content.indexOf("</aside>", openPos+openLen)
+
+//   if (nextOpenPos != -1 && nextOpenPos < nextEndPos){
+
+//   }
+// }
+
+
+// // Loop through each match
+// matchResults.forEach(match => {
+//   // Extract the substring matched by the capturing group
+//   const grupo = match[1].replace(/\r?\n$/, '');
+  
+//   // Replace line breaks with '> ' to format the substring as a blockquote
+  
+//   // Replace the original match with the parsed content in the updated content
+//   content = content.replace(match[0], parsed);
+// });
+
+// // Write the updated content back to the file
+
+// // Print a message indicating the replacement was done
+// if (debug) console.log(`[+] Replacement performed in: ${filePath}`);
+
+// // Error check
+// /<\/?aside>/g.test(updatedContent) && errorLog.push({file: filePath, error: "There are still '<aside>' tags in the file, this can be because of callout that were inside other callouts"})
